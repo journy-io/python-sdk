5d5eb54cabac277662857884fe37041506ce151f
Chato De Veirman <chato@deveirman.it>
2021-02-14 16:27:42 +0100

    Update publish worker

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="journyio-python-sdk",
+    name="journyio-sdk",
     version="0.0.1",
     author="journy.io",
     author_email="hello@journy.io",
